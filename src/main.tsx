6df230f167a3f2d059c74989b4a4b2b920ffafbb
AntonioWork <144033234+AntonioWork@users.noreply.github.com>
2024-07-05 01:19:02 +0300

    feat: removed unneeded imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import {AppV3} from "./AppV3.tsx";
 import {AppV4} from "./AppV4.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
